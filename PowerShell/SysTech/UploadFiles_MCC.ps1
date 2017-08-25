22d83974760a282d8c2154d8eeee776eb3cc4ae7
bj- <bmw518@bk.ru>
2017-08-25 18:10:16 +0300

    UploadMCC: Upload all files from spec folder to Server (SMB)

---
+++ PowerShell/SysTech/UploadFiles_MCC.ps1
@@ -0,0 +1,22 @@
+﻿$UploadPath = "\\shturman\MCC$"
+$SrcPath = "D:\Share\MCC_DB\"
+$username = "SHTURMAN\Upload"
+$pass = "Chi79Mai"
+
+
+$Files = Get-ChildItem -Path $SrcPath -Force | % { $_.FullName }
+
+net use /D $UploadPath
+net use $UploadPath /u:$username $pass
+
+
+"Start move"
+foreach ($OneFile in $Files)
+{
+    Move-Item -Path $OneFile -Destination $UploadPath -Force # -Credential (Get-Credential $objCred)
+}
+"Copied"
+
+Start-Sleep -Seconds 10
+
+net use /D $UploadPath
