2fd1c9e92eaf46288d2a842a5847a404e44537c8
bj- <bmw518@bk.ru>
2020-05-26 09:05:03 +0300

    Update BackUpSettings.ps1
    Set default values

--- PowerShell/SysTech/BackUpSettings.ps1
+++ PowerShell/SysTech/BackUpSettings.ps1
@@ -1,41 +1,41 @@
 # Файл настроек скрипта BackUp.ps1
 
 	# +==============+
-    # |   Log Files  |
+	# |   Log Files  |
 	# +==============+
 	[switch] $Log                  = $FALSE 					     # Бэкап и обслуживание Log файлов ( бех этого колюча остальные из группы игнорируются)
 	[string] $LogFilePath          = "D:\Shturman\Bin\Log"
 	[string] $LogFilePathOld       = "D:\Shturman\Bin\Log\Old"  
 	[switch] $PurgeLogFiles        = $TRUE		                     # похоронить архивы старше  $LogFilePurgeDays дней
-    # Errors log Archiver 
+	# Errors log Archiver 
 	[switch] $Errors               = $TRUE						     # Архивирование Errors файлов
 	[string] $ErrorsPath           = "D:\Shturman\Bin\Errors"	  	 # Папка где лежат Errors, запакует все в каталог $LogFilePathOld\Errors с именем Errors_yyyy_MM_dd.7z
 
 
-    # +==============+
+	# +==============+
 	# |    MySQL     |
 	# +==============+
-	[switch] $MySQL                    = $TRUE					                    # Бэкап и обслуживание MySQL ( без этого колюча остальыне из группы MySQL* игнорируются)
-    [string] $MySQLDumperPath          = "C:\Redmine\mysql\bin\mysqldump.exe"		# То чем создавать дампы
-    [array]  $MySQLCred                = ("backup","E5-Pk+Tz-rz=aT!Ze@Tw")		    # Логин и пароль для заливки
-	[array]  $MySQL_DB                 = ("r2d2","shturman")                        # Список баз для бэкапа
+	[switch] $MySQL                    = $FALSE					                    # Бэкап и обслуживание MySQL ( без этого колюча остальыне из группы MySQL* игнорируются)
+	[string] $MySQLDumperPath          = "C:\Redmine\mysql\bin\mysqldump.exe"		# То чем создавать дампы
+	[array]  $MySQLCred                = ("login","password")		    # Логин и пароль для заливки
+	[array]  $MySQL_DB                 = ("db_name_1","db_name_2")                        # Список баз для бэкапа
 
 	# +==============+
 	# |     SVN      |
 	# +==============+
-	[switch] $SVN            = $TRUE				    # Бэкап и обслуживание SVN ( без этого колюча остальные из группы SVN* игнорируются)
+	[switch] $SVN            = $FALSE				    # Бэкап и обслуживание SVN ( без этого колюча остальные из группы SVN* игнорируются)
 	[string] $SVNRepoPath    = "C:\Repositories"
 	[string] $SVNBackUpPath  = "C:\BackUp\SVN"
 
 	# +==============+
 	# |    Redmine   |
 	# +==============+
-	[switch] $Redmine             = $TRUE			                        # Бэкап и обслуживание Redmine ( бех этого колюча остальные из группы Redmine* игнорируются)
+	[switch] $Redmine             = $FALSE			                        # Бэкап и обслуживание Redmine ( бех этого колюча остальные из группы Redmine* игнорируются)
 	[string] $RedminePath         = "C:\redmine\apps\redmine\htdocs\files"  # Место где живут файлы которые необходимо забекапить (аттачи)
 
 
 	# +=========================+
-    # |    Files and folders    |
+	# |    Files and folders    |
 	# +=========================+
 	#[switch]$FilesON = $TRUE,		                                         # Создавать бекапы файлов/каталогов
 	[switch] $FilesON = $FALSE		                                         # Создавать бекапы файлов/каталогов
@@ -54,7 +54,7 @@
                                 )		# фолдеры целиком
 	# Удаление старых архивов (как и логов) или нет?
 	[string] $FilesExportPath          = "D:\BackUp\2Tape"
-    [switch] $FilesExport              = $TRUE			                  # Выложить последний файл в каталог для экспорта (хардлинк по возможности)
+	[switch] $FilesExport              = $TRUE			                  # Выложить последний файл в каталог для экспорта (хардлинк по возможности)
 
 
 
@@ -70,9 +70,9 @@
 
 
 	# +==============+
-    # |    Common    |
+	# |    Common    |
 	# +==============+
 
-    [string]$BackUpPath = "C:\BackUp"          # каталог для бекапов по умолчанию
-    [string]$ExportPath = "C:\BackUp\2Tape"    # каталог для экспорта бекапов по умолчанию
-    [string]$Export = $TRUE                    # Если включено то все бэкапы будут экспортиться. независимо от местных настроек
+	[string]$BackUpPath = "C:\BackUp"          # каталог для бекапов по умолчанию
+	[string]$ExportPath = "C:\BackUp\2Tape"    # каталог для экспорта бекапов по умолчанию
+	[string]$Export = $TRUE                    # Если включено то все бэкапы будут экспортиться. независимо от местных настроек
